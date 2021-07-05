0f5ce2a4badead8553d6086c2b3afa32f6bcee00
mnerisilva <marceloneri@gmail.com>
2021-07-05 15:29:46 -0300

    update

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 700;
 
-enemy1 = {
+/*enemy1 = {
     x: 50,
     y: 100,
     width: 200,
     height: 200
+}*/
+
+class Enemy {
+    constructor(){
+        this.x = 50;
+        this.y = 100;
+        this.width = 200;
+        this.height = 200;
+    }
 }
+
+const enemy2 = new Enemy();
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    //enemy1.x++;
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
-    //requestAnimationFrame(animate);
+    enemy2.x++;
+    ctx.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height);
+    requestAnimationFrame(animate);
 }
 
-document.addEventListener('keydown', function(event){
+/*document.addEventListener('keydown', function(event){
     const keyName = event.key;
     //console.log('keydown event\n\n' + 'key: ' + keyName);
     if(keyName == 'ArrowRight'){
@@ -30,6 +41,6 @@ document.addEventListener('keydown', function(event){
         animate();
         console.log(enemy1.y);
     }
-});
+});*/
 
 animate();
